02da88965307bee96a320197f458069cb62a9941
Lance Parsons <lparsons@genomics.princeton.edu>
2015-11-18 12:20:00 -0500

    Set 503 message via ansible

--- roles/galaxy/files/galaxy_stop.sh
+++ roles/galaxy/files/galaxy_stop.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Set 503 to maintenance
-ln -sf "${DIR}/../www/html/error-documents/503_scheduled_maintenance.html" "${DIR}/../www/html/error-documents/503.html"
 
 # Set GALAXY_RUN_ALL to start all web and handler processes defined
 export GALAXY_RUN_ALL=1
